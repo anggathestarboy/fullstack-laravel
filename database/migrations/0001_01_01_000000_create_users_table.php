c7fb7c2e8824a43076057ee14d05481508bae321
Its Anggara <anggarathestarboy@gmail.com>
2025-09-22 09:41:25 +0700

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Mau copy ya?, tidak semudah itu
      */
     public function up(): void
     {
